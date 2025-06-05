e255f565577903c7311d7b18a52b7fedc5b406ac
Lupova19 <nadiqlupova@gmail.com>
2025-06-05 23:44:45 +0300

    Нов комит

---
+++ DeliveryZadacha/Models/DeliveryContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeliveryZadacha
+{
+    public class DeliveryContext : DbContext
+    {
+        public DeliveryContext() : base("DeliveryContext")
+        {
+
+        }
+        public DbSet<DishTypes> DishTypes { get; set; }
+        public DbSet<Dish> Dishes { get; set; }
+    }
+}
